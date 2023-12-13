d47e552ff671e9e267aa9ff0a53e39d693ccd93f
Sean Gerhardt <sean_gerhardt@hotmail.com>
2023-12-12 20:34:04 -0600

    expand space as needed

--- day11/stars.go
+++ day11/stars.go
@@ -24,35 +24,61 @@ func part1(filename string) {
 	var input [][]string
 	idx := 0
 	var emptyRowIndexes []int
+	var emptyColIndexes []int
 	for scanner.Scan() {
 		line := scanner.Text()
-		print(line)
+		//print(line)
 		if !strings.Contains(line, "#") {
 			// we found an empty row
 			emptyRowIndexes = append(emptyRowIndexes, idx)
 		}
+		input = append(input, strings.Split(line, ""))
 		idx++
 	}
 
-	// double the empty rows now...
-
-	for idx, emptyRow := range emptyRowIndexes {
-		addEmptyRow(input, emptyRow)
+	// check for cols that only contain "."
+	for col := 0; col < len(input[0]); col++ {
+		emptyCol := true
+		for row := 0; row < len(input); row++ {
+			if input[row][col] != "." {
+				emptyCol = false
+				break
+			}
+		}
+		if emptyCol {
+			emptyColIndexes = append(emptyColIndexes, col)
+		}
 	}
+
+	// double the empty rows now...
+	for i, emptyRow := range emptyRowIndexes {
+		addEmptyRow(input, emptyRow+i)
+	}
+
+	for _, emptyCol := range emptyColIndexes {
+		addEmptyCol(input, emptyCol)
+	}
+
+	for _, rows := range input {
+		for _, val := range rows {
+			print(val)
+		}
+		println()
+	}
+
 }
 
 func addEmptyRow(input [][]string, idx int) [][]string {
-
 	emptyRow := make([]string, len(input[0]))
 	for i := 0; i < len(input[0]); i++ {
 		emptyRow[i] = "."
 	}
+	return slices.Insert(input, idx, emptyRow)
+}
 
-	for i := 0; i < len(input); i++ {
-		println(input[i])
-
-		slices.Insert(input, idx, emptyRow)
+func addEmptyCol(input [][]string, idx int) [][]string {
+	for row := 0; row < len(input); row++ {
+		input[row] = slices.Insert(input[row], idx, ".")
 	}
-
 	return input
 }
