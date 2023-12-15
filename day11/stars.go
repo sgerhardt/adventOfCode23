d00ff9b239454c59b543fad4e024c7c54af5967e
Sean Gerhardt <sean_gerhardt@hotmail.com>
2023-12-14 19:40:36 -0600

    WIP

--- day11/stars.go
+++ day11/stars.go
@@ -10,7 +10,9 @@ import (
 )
 
 func main() {
-	part1("day11/stars.txt")
+	filename := "day11/test.txt"
+	//part1(filename)
+	part2(filename)
 }
 
 func part1(filename string) {
@@ -138,4 +140,84 @@ func calcDistance(position1, position2 position) int {
 	}
 	// for each rise, we add two to the run until we reach the rise, then we add one to the run until we hit the target
 	return 2*rise + (run - rise)
+}
+
+func part2(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var input [][]string
+	idx := 0
+	var emptyRowIndexes []int
+	var emptyColIndexes []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.Contains(line, "#") {
+			// we found an empty row
+			emptyRowIndexes = append(emptyRowIndexes, idx)
+		}
+		input = append(input, strings.Split(line, ""))
+		idx++
+	}
+
+	// check for cols that only contain "."
+	for col := 0; col < len(input[0]); col++ {
+		emptyCol := true
+		for row := 0; row < len(input); row++ {
+			if input[row][col] != "." {
+				emptyCol = false
+				break
+			}
+		}
+		if emptyCol {
+			emptyColIndexes = append(emptyColIndexes, col)
+		}
+	}
+
+	for i, emptyRow := range emptyRowIndexes {
+		input = addMultipleEmptyRows(input, emptyRow+(99*i), 99)
+	}
+
+	for i, emptyCol := range emptyColIndexes {
+		input = addMultipleEmptyCols(input, emptyCol+(99*i), 99)
+	}
+	//for _, rows := range input {
+	//	for _, val := range rows {
+	//		print(val)
+	//	}
+	//	println()
+	//}
+
+	galaxies := gatherGalaxies(input)
+	sum := 0
+	for i := 0; i < len(galaxies); i++ {
+		// start at i+1 to avoid double counting
+		for j := i + 1; j < len(galaxies); j++ {
+			g := galaxies[i]
+			otherGalaxy := galaxies[j]
+			distance := calcDistance(g.pos, otherGalaxy.pos)
+			sum += distance
+			fmt.Printf("Between galaxy %d and galaxy %d from galaxy: %d \n", g.number, otherGalaxy.number, distance)
+		}
+	}
+	fmt.Println("Sum of distances:", sum)
+}
+
+func addMultipleEmptyRows(input [][]string, idx int, num int) [][]string {
+	for i := 0; i < num; i++ {
+		input = addEmptyRow(input, idx+i)
+	}
+	return input
+}
+
+func addMultipleEmptyCols(input [][]string, idx int, num int) [][]string {
+	for i := 0; i < num; i++ {
+		input = addEmptyCol(input, idx+i)
+	}
+	return input
 }
